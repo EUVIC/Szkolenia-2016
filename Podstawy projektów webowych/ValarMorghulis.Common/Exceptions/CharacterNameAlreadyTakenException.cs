a83359814dc116fac8fdfe762740c91019910447
Szymon Graczyk <szymon.graczyk@euvic.pl>
2016-07-06 21:08:39 +0200

    Dodanie wyjątków na potrzeby duplikatu nazwy oraz braku rekordu w bazie.

---
+++ Podstawy projektów webowych/ValarMorghulis.Common/Exceptions/CharacterNameAlreadyTakenException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValarMorghulis.Common.Exceptions
+{
+	public class CharacterNameAlreadyTakenException : Exception
+	{
+		public CharacterNameAlreadyTakenException() { }
+		public CharacterNameAlreadyTakenException(string message) : base(message) { }
+		public CharacterNameAlreadyTakenException(string message, Exception inner) : base(message, inner) { }
+	}
+}
